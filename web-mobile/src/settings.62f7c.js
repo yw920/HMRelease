a8ae6178eeb4d4de6e0470b8f8c51b875513c4a2
yanwei.920 <yw920@icloud.com>
2021-06-23 15:46:32 +0800

    v1.2

--- web-mobile/src/settings.62f7c.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",Prefab:"4dd7b",Texture:"c945f",main:"5b5be"}};
