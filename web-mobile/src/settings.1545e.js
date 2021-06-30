c50a0eda0d54f66ec921c31ea0705619fda3dee3
yanwei.920 <yw920@icloud.com>
2021-06-30 20:39:04 +0800

    change timer implement

---
+++ web-mobile/src/settings.1545e.js
@@ -0,0 +1,25 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/main.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "d40d2",
+        Prefab: "470aa",
+        Texture: "a3a35",
+        main: "a80e6"
+    }
+};
