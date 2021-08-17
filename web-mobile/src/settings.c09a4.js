a37d17d0813685def0642b48d0e1b1f5d9de6770
yanwei.920 <yanwei.920@bytedance.com>
2021-08-17 11:00:14 +0800

    add some logs and close ws and web audio

---
+++ web-mobile/src/settings.c09a4.js
@@ -0,0 +1,25 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/main.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "d40d2",
+        Prefab: "7a8fc",
+        Texture: "f8f65",
+        main: "3abe4"
+    }
+};
